64551bbfacd7651d640525dd9e82eb6a2bb991b7
belinhaa <149503185+belinhaa@users.noreply.github.com>
2024-12-01 11:18:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+﻿let happiness = 100;
+
+function updateStatus() {
+    const statusText = document.getElementById('statusText');
+    const tamaImage = document.getElementById('tamaImage');
+
+    if (happiness > 75) {
+        statusText.textContent = 'Muito Feliz';
+        tamaImage.src = 'desenho.png';  // Imagem quando feliz
+    } else if (happiness > 50) {
+        statusText.textContent = 'Feliz';
+        tamaImage.src = 'desenho.png';  // Imagem quando feliz
+    } else if (happiness > 25) {
+        statusText.textContent = 'Triste';
+        tamaImage.src = 'desenho(1).png';  // Imagem quando triste
+    } else {
+        statusText.textContent = 'Muito Triste';
+        tamaImage.src = 'desenho(1).png';  // Imagem quando triste
+    }
+}
+
+function feed() {
+    happiness += 10;
+    if (happiness > 100) happiness = 100;
+    updateStatus();
+}
+
+function clean() {
+    happiness += 5;
+    if (happiness > 100) happiness = 100;
+    updateStatus();
+}
+
+function play() {
+    happiness += 15;
+    if (happiness > 100) happiness = 100;
+    updateStatus();
+}
